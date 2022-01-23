75cd2af297bf94b270098e1891e9a5fade95c5a7
Aleksandar Popov <v-apopov@microsoft.com>
2022-01-23 16:39:14 +0100

    Project

---
+++ DictionaryApp/Months.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryApp
+{
+    class Months
+    {
+        private string monthName { get; set; }
+
+        public Months(string monthname)
+        {
+            this.monthName = monthname;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Month Name:{0}",  monthName);
+        }
+    }
+}
---
+++ DictionaryApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // StringComparer.InvariantCultureIgnoreCase is used so that we can get specific value no mather if key is lower or uper case
+            Dictionary<string, Months> months = new Dictionary<string, Months>(StringComparer.InvariantCultureIgnoreCase)
+            {    
+                {"A",new Months("January")},
+                {"B",new Months("February")},
+                {"C",new Months("March")},
+
+            };
+
+            // Write keys and values
+            foreach (var key in months)
+                Console.WriteLine(key);
+            Console.WriteLine();
+
+            // Write values for all keys
+            foreach (var key in months)
+                Console.WriteLine(key.Value);
+            Console.WriteLine();
+
+            // Write keys without value
+            foreach (var key in months)
+                Console.WriteLine(key.Key);
+            Console.WriteLine();
+
+            // Write value for given key
+            Console.WriteLine(months["b"]);
+            Console.ReadLine();
+        }
+    }
+}
